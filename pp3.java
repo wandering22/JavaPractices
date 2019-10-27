e7af2b9d4e8d0f911d80a4e87ea04ad159963ab9
wandering <2249655703@qq.com>
2019-10-27 23:01:33 +0800

    提交代码

---
+++ pp3.java
@@ -0,0 +1,56 @@
+public class pp3{
+    public static void main(String[] args){
+		//String s="Hello World";
+		//System.out.println(s);
+		/*System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args.length);*/
+		/*String a="你好";
+		String b="世界";
+		System.out.println("\""+a+b+"\"");*/
+		/*if (args.length<2)
+		{
+			System.out.println("请输入");
+		return;}
+		int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+		int result=x+y;
+		System.out.println(""+x+"+"+y+"相加的结果是"+result);
+		System.out.printf("%d+%d 相加的结果是 %n",x,y,result);
+		System.out.print(""+x+"+"+y+"相加的结果是"+result);//不换行*/
+		
+		//String 和其他类型的相互转化
+		//String转化为其他
+	    /*String s="12345";
+		System.out.println(Integer.parseInt(s));
+		System.out.println(Long.parseLong(s));
+		System.out.println(Double.parseDouble(s));*/
+		//其他转化为String
+		/*int a=10;
+		long b=20L;
+		double c=3.1415;
+		System.out.println(b);
+		System.out.println(String.valueOf(a));
+		System.out.println(String.valueOf(b));
+		System.out.println(String.valueOf(c));*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+}
