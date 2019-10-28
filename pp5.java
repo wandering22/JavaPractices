63318fac3e66e3d22d582a1b679265dd81d1eac8
wandering <2249655703@qq.com>
2019-10-28 15:14:02 +0800

    运算符练习

---
+++ pp5.java
@@ -0,0 +1,19 @@
+public class pp5{
+	public static void main(String[] args){
+/*int i=3;
+int j=0;
+System.out.println(i/j);*/
+double m=3.0;
+double n=0.0;
+System.out.println(m/n);
+	int a = 10; 
+	int b = 20; 
+	System.out.println(a == b); 
+	System.out.println(a != b);
+	System.out.println(a < b); 
+	System.out.println(a > b);
+	System.out.println(a <= b); 
+	System.out.println(a >= b);
+	
+	}
+}
