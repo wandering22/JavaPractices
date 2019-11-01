56d3204222fdd6ff30cf449ee24a4fed534ae69c
wandering <2249655703@qq.com>
2019-11-01 14:46:45 +0800

    nextLine

---
+++ pp10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class pp10{
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		while (scanner.hasNext()){
+			String s=scanner.next();
+			//String s=scanner.nextLine();
+			System.out.println(s);
+		}
+	}
+}
