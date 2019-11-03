3c092908a0cc98e475b6d5ae4e0c2edb2dbd62de
wandering <2249655703@qq.com>
2019-11-03 21:10:07 +0800

    减法

---
+++ pp15.java
@@ -0,0 +1,14 @@
+public class pp15{
+	
+		//计算减法
+		public static double sub(double a,double b){
+			double i=a-b;
+			return i;
+		}
+		public static void main(String[] args){
+			double a=5;
+			double b=3;
+			double i=sub(a,b);
+			System.out.println(i);
+		}
+}
