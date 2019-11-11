649199a4786f80cd71636fb22d4f933f4af112fd
wandering <2249655703@qq.com>
2019-11-11 20:48:42 +0800

    类和对象 自行车

---
+++ CycleDemo.java
@@ -0,0 +1,9 @@
+public class CycleDemo{
+	public static void main(String[] args){
+		Cycle cycle=new Cycle();
+		System.out.println(cycle.getcolor());
+		System.out.println(cycle.getspeed());
+		
+		Cycle.setcolor="粉色";
+	}
+}
