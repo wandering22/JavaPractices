1118c07c7f653ec1e985e329ae38b3ca387b700d
wandering <2249655703@qq.com>
2019-11-12 19:28:30 +0800

    左旋

---
+++ Revolve.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class Revolve{
+	
+	public static int[] spin(int[]a,int n){
+		int i=0;
+		int[] b={0,0,0,0,0,0,0};
+		//int k=a.length-n-1;
+			if(n>a.length){
+			n=n-a.length;}
+			for(int j=n;j<a.length;j++){
+			
+				b[i]=a[j];
+				i++;	
+		}
+		for(int j=0;j<n;j++){
+				b[i]=a[j];
+				i++;
+			}
+		
+		return b;
+	}
+	public static void main(String[] args){
+		int[] a={1,2,3,4,5,6,7};
+		int n=11;
+		int[] b=spin(a,n);
+		System.out.println(Arrays.toString(b));
+	}
+}
+		
