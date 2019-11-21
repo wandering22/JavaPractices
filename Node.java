de9d39ad7c83555cdc773b1bf15ff25bfd6f9453
wandering <2249655703@qq.com>
2019-11-21 17:22:42 +0800

    链表

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node{
+	int val;
+	Node next;
+	
+	Node (int val,Node next){
+		this.val=val;
+		this.next=next;
+	}
+	
+	Node (int val){
+		this(val,null);
+	}
+	@Override
+	public String toString(){
+		return String.format("Null{%d}",val);
+	}
+}
