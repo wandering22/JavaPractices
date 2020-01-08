9fe47c8db4bf9b01e4d1b98239e2bd101cf2c519
wandering <2249655703@qq.com>
2020-01-08 20:50:38 +0800

    提交

---
+++ MinStack1.java
@@ -0,0 +1,35 @@
+﻿import java.util.Stack;
+
+public class MinStack1 {
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+    /** initialize your data structure here. */
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+    }
+
+    public void push(int x) {
+        if(minSt.isEmpty() || x <= minSt.peek()){
+            minSt.push(x);
+        }
+        st.push(x);
+    }
+
+    public void pop() {
+        int ret = st.peek();
+        //自动拆箱
+        if(ret == minSt.peek()){
+            minSt.pop();
+        }
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public int getMin() {
+        return minSt.peek();
+    }
+}
