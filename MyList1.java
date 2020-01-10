061dcb7c7d93378c233c5e270775c9c24216e5f4
wandering <2249655703@qq.com>
2020-01-10 12:36:48 +0800

    提交

---
+++ MyList1.java
@@ -0,0 +1,32 @@
+﻿
+public interface MyList1 {
+    void pushFront(int element);
+    void pushBack(int element);
+    void insert(int index, int element);
+}
+
+abstract class AbstractList implements MyList {
+    protected int size;
+
+    @Override
+    public void pushFront(int element) {
+        insert(0, element);
+    }
+
+    @Override
+    public void pushBack(int element) {
+        insert(size, element);
+    }
+}
+
+class MyArrayList extends AbstractList {
+    @Override
+    public void insert(int index, int element) {
+    }
+}
+
+class MyLinkedList extends AbstractList {
+    @Override
+    public void insert(int index, int element) {
+    }
+}
