3cb1ef70a7df3049d22e5edfea47c5e6075fefa3
wandering <2249655703@qq.com>
2020-01-20 21:57:05 +0800

    提交

---
+++ UseList22.java
@@ -0,0 +1,14 @@
+﻿package 演示接口的使用;
+
+public class UseList22 {
+    public static MyList selectYiFang() {
+        return new MyLinkedList();
+    }
+
+    public static void main(String[] args) {
+        MyList list = selectYiFang();
+        list.pushFront(1);
+        list.pushFront(2);
+        list.popFront();
+    }
+}
