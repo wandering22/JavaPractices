13013cdaea04cd0360c1be1aaa0c1f3eb163f1cf
wandering <2249655703@qq.com>
2020-01-21 22:06:40 +0800

    提交

---
+++ Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    private static int method() {
+        return 100;
+    }
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+    }
+}
