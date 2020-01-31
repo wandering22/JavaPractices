84c25df210d7be08235c7386ee7621ea1c5a2ef7
wandering <2249655703@qq.com>
2020-01-31 21:58:43 +0800

    提交

---
+++ LengthOfLastWord.java
@@ -0,0 +1,15 @@
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else if (length != 0) {
+                return length;
+            }//else if(length==0){
+            //     return 0;
+            // }
+        }
+        return length;
+    }
+}
