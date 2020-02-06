df9526993aafe4b6dc53f703dcd7a81144eda5ec
wandering <2249655703@qq.com>
2020-02-06 14:11:15 +0800

    提交

---
+++ ContainsDuplicate2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ContainsDuplicate2 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i+1] == nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
