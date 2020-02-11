eb4e39e3e602af27b01118e482a2e12b5b3bbea4
wandering <2249655703@qq.com>
2020-02-11 18:25:16 +0800

    提交

---
+++ SortedSquares.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SortedSquares {
+    public int[] sortedSquares(int[] A) {
+        int[] B=new int[A.length];
+
+        for(int i=0;i<A.length;i++){
+
+            B[i]=A[i]*A[i];
+        }
+        Arrays.sort(B);
+        return B;
+    }
+}
