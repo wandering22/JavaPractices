5726d41f2611374fffec3f44072266be772561dd
wandering <2249655703@qq.com>
2020-02-15 21:02:38 +0800

    提交

---
+++ SameTree2.java
@@ -0,0 +1,16 @@
+
+public class SameTree2 {
+//判断两棵树是否相同
+    public boolean isSame(Node root1,Node root2){
+    if(root1==null&&root2==null){
+        return true;
+
+    }
+    if(root1==null||root2==null){
+        return false;
+    }
+    return root1.value==root2.value
+            &&isSame(root1.left,root2.left)
+            &&isSame(root1.right,root2.right);
+    }
+}
