0793ed6ab60b4c6b097bc58b2ae67b156b6833ee
wandering <2249655703@qq.com>
2020-03-11 21:58:59 +0800

    提交

---
+++ LastStoneWeight.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        int len = stones.length - 1;
+        for (int i = len; i > 0; i--) {
+            stones[len] = stones[len] - stones[len - 1];
+            stones[len - 1] = 0;
+            Arrays.sort(stones);
+        }
+        return stones[len];
+    }
+}
