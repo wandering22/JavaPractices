8d82c1205037c38e14f411a58ddc565ce18c7c66
wandering <2249655703@qq.com>
2020-03-31 15:52:05 +0800

    线程安全

---
+++ 演示线程不安全.java
@@ -0,0 +1,33 @@
+public class 演示线程不安全 {
+    private static long n = 0;
+
+    static class Add extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100000; i++) {
+                n++;
+            }
+        }
+    }
+
+    static class Sub extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100000; i--) {
+                n--;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Sub sub = new Sub();
+        sub.start();
+        Add add = new Add();
+        add.start();
+
+        add.join();
+        sub.join();
+
+        System.out.println(n);  // 我期望中 n 的值应该是多少
+    }
+}
