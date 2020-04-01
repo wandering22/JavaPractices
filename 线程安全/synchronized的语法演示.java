d435859b27b90d6232e821386a3c82301054ef7d
wandering <2249655703@qq.com>
2020-04-01 20:06:01 +0800

    线程安全

---
+++ 线程安全/synchronized的语法演示.java
@@ -0,0 +1,26 @@
+package 线程安全;
+
+public class synchronized的语法演示 {
+    // 作为方法修饰符
+    public synchronized static void staticMethod() {
+        /*
+        synchronized (synchronized的语法演示.class) {
+        }
+        */
+    }
+
+    public synchronized void method() {
+        /*
+        synchronized (this) {
+        }
+        */
+    }
+
+    // 作为代码块 —— 同步代码块
+    public void someMethod() {
+        Object o = new Object();
+        synchronized (o) {
+        }
+    }
+}
+
