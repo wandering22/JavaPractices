07a1716fed7f21002b340762ac7cd57418a1cd97
wandering <2249655703@qq.com>
2020-04-02 12:42:15 +0800

    提交

---
+++ 使用java提供的方式通知停止/B.java
@@ -0,0 +1,21 @@
+package 使用java提供的方式通知停止;
+
+import java.util.concurrent.TimeUnit;
+
+public class B extends Thread {
+    @Override
+    public void run() {
+        // B 线程中判断是否有人让自己停止
+        while (Thread.interrupted() == false) {
+            try {
+                System.out.println("挖煤");
+                TimeUnit.SECONDS.sleep(1000);
+            } catch (InterruptedException e) {
+                // 我收到让我停止的通知了，但我就不停
+                System.out.println(e);
+                // 如果 B 要停止，就是退出循环，怎么退出？
+                break;
+            }
+        }
+    }
+}
