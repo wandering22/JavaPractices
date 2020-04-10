0eb2692dca253eeb000b97830174a65b8e3d63af
wandering <2249655703@qq.com>
2020-04-10 14:51:54 +0800

    练习

---
+++ MagicPocket.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MagicPocket {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+
+        int i=0;
+        while(scanner.hasNextInt()){
+
+            arr[i]=scanner.nextInt();
+
+        }
+        //for(int a=0;a++;a<arr.length){
+
+        //}
+    }
+//    public static int install(int n,int k){
+//        if(n==0){
+//            return 0;
+//        }
+//
+//    }
+}
