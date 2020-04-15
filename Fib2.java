c8a626e49909c94cde87da870f51ee1d4aa8f1ea
wandering <2249655703@qq.com>
2020-04-15 21:22:52 +0800

    练习

---
+++ Fib2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fib2 {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N=in.nextInt();
+        int a=0,b=1;
+        while(b<=N){
+            int b_temp=b+a;
+            a=b;
+            b=b_temp;
+        }
+        System.out.print((b-N)>(N-a)?N-a:b-N);
+    }
+}
