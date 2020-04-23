eb007102a75668870ac4405a954c41d54f0c056c
wandering <2249655703@qq.com>
2020-04-23 21:43:00 +0800

    提交

---
+++ udp/v1/v3/Server.java
@@ -0,0 +1,42 @@
+package udp.v1.v3;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        // 开一家店
+        ServerSocket serverSocket = new ServerSocket(9898);
+
+        // 循环处理业务
+        while (true) {
+            Socket socket = serverSocket.accept();  // 虽然有 4 个Client 连接，socket 代表的是第一个Client
+
+            // 获取输入流
+            InputStream is = socket.getInputStream();
+            // 封装成 Scanner
+            Scanner scanner = new Scanner(is, "UTF-8");
+            // 使用 \r\n 进行分割的方式，读取一个请求
+
+            // 等着第一个 Client 发送请求
+            String request = scanner.nextLine();    // nextLine() 把 \r\n 已经去掉
+            System.out.println("收到请求: " + request);
+
+            // 业务处理
+            String response = request;
+
+            // 发送响应，也许要使用 \r\n 跟在后边，进行分割
+            OutputStream os = socket.getOutputStream();
+            // 封装成 PrintWriter
+            PrintWriter writer = new PrintWriter(
+                new OutputStreamWriter(os, "UTF-8")
+            );
+            // 发送响应
+            writer.println(response);   // println 会帮我们在后边增加 \r\n
+            writer.flush();
+
+            socket.close();
+        }
+    }
+}
