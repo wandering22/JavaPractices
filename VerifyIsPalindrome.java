65ba33fabadee6bb2b8c98f082aa4edebed7a326
wandering <2249655703@qq.com>
2020-04-30 21:37:00 +0800

    练习

---
+++ VerifyIsPalindrome.java
@@ -0,0 +1,14 @@
+public class VerifyIsPalindrome {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        int l = s.length();
+        StringBuilder str = new StringBuilder(l);
+        for (char c : s.toCharArray()) {
+            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
+                str.append(c);
+            }
+        }
+        return str.toString().equals(str.reverse().toString());
+    }
+}
