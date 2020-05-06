01df69cc476fb3d1bd3b1abe5bd2a49f3d876a2b
wandering <2249655703@qq.com>
2020-05-06 12:15:29 +0800

    练习

---
+++ HardestQuestion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HardestQuestion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c >= 'A' && c < 'F') {
+                    System.out.print((char) ((int) c + 21));
+                } else if (c > 'E' && c <= 'Z') {
+                    System.out.print((char) ((int) c - 5));
+                } else {
+                    System.out.print(s.charAt(i));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
