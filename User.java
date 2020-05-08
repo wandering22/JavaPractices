78ebf29cefc7f41badebfc9d0a1ea7cf5834ac9b
wandering <2249655703@qq.com>
2020-05-08 21:47:40 +0800

    提交

---
+++ User.java
@@ -0,0 +1,13 @@
+public class User {
+    String username;
+    String nickname;
+    String password;
+    long balance;
+
+    public User(String username, String nickname, String password, long balance) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+        this.balance = balance;
+    }
+}
