6f3073171fdb7b580d6ff80f7001436851fb7661
wandering <2249655703@qq.com>
2020-05-15 22:46:13 +0800

    提交

---
+++ FakeJSP.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class FakeJSP {
+    public static void main(String[] args) {
+        // 1. 根据 URL 找到对应的 JSP 文件
+        // 2. 读取我们写的 JSP 文件
+        // 3. 按行处理 - 根据不同的开头 <% 不同的处理方式
+        // 4. 写入一个新的文件中 publish_jsp.java
+        // 5. 编译该文件
+        // 6. 把 URL 请求交给该 servlet 处理
+    }
+}
