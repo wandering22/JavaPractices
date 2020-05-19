8c4eae975cd03ba4cc8855b25dac5d09fe6e61b2
wandering <2249655703@qq.com>
2020-05-19 23:53:43 +0800

    提交

---
+++ Balance.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+public class Balance {
+    public boolean isBalance(TreeNode root) {
+        if(root==null){
+            return true;
+        }else{
+            int left = getDeep(root.left);
+            int right = getDeep(root.right);
+            if(Math.abs(left-right)<=1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public int getDeep(TreeNode root){
+        if(root!=null){
+            int left = getDeep(root.left);
+            int right = getDeep(root.right);
+            return left>right?left+1:right+1;
+        }else{
+            return 0;
+        }
+    }
+}
