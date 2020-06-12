a61c39f3d5a60393808b7c4a3c313d70f4230bd1
wandering <2249655703@qq.com>
2020-06-12 22:19:36 +0800

    练习

---
+++ NearestPublicGrand.java
@@ -0,0 +1,14 @@
+public class NearestPublicGrand {
+    public int getLCA(int a, int b) {
+        // write code here
+
+        while(a != b)
+        {
+            if(a > b)
+                a /= 2;
+            else
+                b /= 2;
+        }
+        return a;
+    }
+}
