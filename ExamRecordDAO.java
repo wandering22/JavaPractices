93a296b067896542d7a86ec84b144dee12568dbe
wandering <2249655703@qq.com>
2020-07-09 16:53:41 +0800

    提交

---
+++ ExamRecordDAO.java
@@ -0,0 +1,91 @@
+package lliuyi.Dao;
+
+import lliuyi.model.*;
+import lliuyi.util.DBUtil;
+
+import javax.servlet.annotation.WebServlet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//@WebServlet("examRecord/query")
+public class ExamRecordDAO {
+    public static List<ExamRecord> query(){
+
+        List<ExamRecord> records=new ArrayList<>();
+        Connection c=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        try {
+            c=DBUtil.getConnection();
+            String sql="select" +
+                    "  er.id," +
+                    "  er.score," +
+                    "  er.student_id," +
+                    "  er.exam_id," +
+                    "  er.exam_record_desc," +
+                    "  er.create_time," +
+                    "  e.id            prefix_e_id," +
+                    "  e.exam_name     prefix_e_exam_name," +
+                    "  e.exam_desc     prefix_e_exam_desc," +
+                    "" +
+                    "  c.id            prefix_c_id," +
+                    "  c.classes_name  prefix_c_classes_name," +
+                    "" +
+                    "  c2.id           prefix_c2_id," +
+                    "  c2.course_name  prefix_c2_course_name," +
+                    "  s.id            prefix_s_id," +
+                    "  s.student_name  prefix_s_student_name," +
+                    "  s.student_no    prefix_s_student_no," +
+                    "  s.id_card       prefix_s_id_card," +
+                    "  s.student_email prefix_s_student_email" +
+                    " from exam_record er" +
+                    "  join exam e on er.exam_id = e.id" +
+                    "  join classes c on e.classes_id = c.id" +
+                    "  join course c2 on e.course_id = c2.id" +
+                    "  join student s on er.student_id = s.id";
+            ps=c.prepareStatement(sql);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                //设置考试成绩对象
+                ExamRecord er=new ExamRecord();
+                er.setId(rs.getInt("id"));
+                er.setScore(rs.getBigDecimal("score"));
+                er.setExamRecordDesc(rs.getString("exam_record_desc"));
+                er.setCreateTime(new Date(rs.getTimestamp("creat_time").getTime()));
+                //设置考试对象
+                Exam e=new Exam();
+                e.setId(rs.getInt("prefix_e_id"));
+                e.setExamName(rs.getString("prefix_e_exam_name"));
+                e.setExamDesc(rs.getString("prefix_e_exam_desc"));
+                er.setExam(e);
+                //设置班级对象
+                Classes classes=new Classes();
+                classes.setId(rs.getInt("prefix_c_id"));
+                classes.setClassesName(rs.getString("prefix_c_classes_name"));
+                er.setClasses(classes);
+                //设置课程对象
+                Course course=new Course();
+                course.setId(rs.getInt("prefix_c2_id"));
+                course.setCourseName(rs.getString("prefix_c2_course_name"));
+                er.setCourse(course);
+                //设置学生对象
+                Student student=new Student();
+                student.setId(rs.getInt("prefix_s_id"));
+                student.setStudentName(rs.getString("prefix_s_student_name"));
+                student.setStudentNo(rs.getString("prefix_s_student_no"));
+                student.setIdCard(rs.getString("prefix_s_id_card"));
+                student.setStudentEmail(rs.getString("prefix_s_student_email"));
+                er.setStudent(student);
+                records.add(er);
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("查询考试列表出错",e);
+        }
+        return records;
+    }
+}
