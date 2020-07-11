9b15193a3b64240246d870f8342c841acde37a9f
wandering <2249655703@qq.com>
2020-07-11 18:15:33 +0800

    提交

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,3 +1,11 @@
+package lliuyi.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lliuyi.Dao.UserDAO;
+import lliuyi.model.Response;
+import lliuyi.model.User;
+import lliuyi.util.JSONUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,25 +13,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/user/login")
+public class LoginServlet extends AbstractBaseServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        doPost(req, resp);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        req.setCharacterEncoding("UTF-8");
+//        resp.setCharacterEncoding("UTF-8");
+//        resp.setContentType("application/json");
+////        resp.setContentType("text/html");
+//        String username = req.getParameter("username");
+//        String password = req.getParameter("password");
+//        System.out.printf("username=%s,password=%s\n",username,password);
+////        PrintWriter pw = resp.getWriter();
+////        pw.println("登陆成功");
+////        pw.flush();
+//        PrintWriter pw = resp.getWriter();
+//        pw.println(new ObjectMapper().writeValueAsString(Response.ok()));
+//        pw.flush();
+//    }
+
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-
-        User user = UserTable.select(username, password);
-        resp.setContentType("text/plain; charset=utf-8");
-        if (user == null) {
-            resp.getWriter().println("用户认证失败，已报警");
-        } else {
-            // 设置 Session 信息
-            // 把用户对象直接保存到 Session 中
-            // API 提供的 Session，会自动种下 Cookie
-            HttpSession session = req.getSession();
-            session.setAttribute("user", user);
-            resp.getWriter().println("用户认证成功，请进行下一步操作");
-        }
+    public Object process (HttpServletRequest req, HttpServletResponse resp)throws Exception {
+        User user = JSONUtil.read(req.getInputStream(),User.class);
+        User queryUser=UserDAO.query(user);
+        if(queryUser==null)
+            throw  new RuntimeException("用户登陆出错");
+        HttpSession session=req.getSession() ;
+        session.setAttribute("user",queryUser);
+        return null;
     }
 }
+
